a821b5afb3b32d6f8b79ced30c30533b6852fda6
unknown <guilhermedeoliveiradesouza@gmail.com>
2023-11-14 08:23:42 -0300

    query params for routes added

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -2,4 +2,7 @@
 @section('title', 'Products')
 @section('content')
     <h1>Tela de produtos</h1>
+    @if ($search != '')
+        <p>O usuário está buscando por: {{ $search }}</p>
+    @endif
 @endsection
